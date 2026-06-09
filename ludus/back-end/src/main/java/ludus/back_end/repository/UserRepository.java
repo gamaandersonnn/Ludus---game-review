7bf2a0e462c1e00243f7086443737b78a60d1f1e
gamaandersonnn <skazzbr@hotmail.com>
2026-06-09 10:47:08 -0300

    gitinore no back

--- ludus/back-end/src/main/java/ludus/back_end/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package ludus.back_end.repository;
-
-import ludus.back_end.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface UserRepository extends JpaRepository<User, String> {
-
-}
